ed7af33fdd709a1d7abc165c39e48e63ca98740d
Sergio Rosa <scarlosjazz@gmail.com>
2019-02-27 08:57:15 -0500

    O(1) Constant Time

--- index.js
+++ index.js
@@ -16,3 +16,14 @@ function findNemo2(fish) {
 }
 
 findNemo2(large)// O(n) --> Linear Time
+
+// =========================O(1)  -> Constant Time ============
+const boxes = [0, 1,2,3,4,5];
+
+function logFirstTwoBoxes(boxes) {
+    console.log(boxes[0]); // O(1)
+    console.log(boxes[1]);// O(1)
+  
+  }
+  
+  logFirstTwoBoxes(boxes) // O(1)  -> Constant Time
