5c2d94758638131f693e2a276a4722792134359b
Sergio Rosa <scarlosjazz@gmail.com>
2019-03-01 12:14:05 -0500

    Javascript-DS_Updated

--- datastructiure-algo.js
+++ datastructiure-algo.js
@@ -1,118 +1,118 @@
-// // log all pairs of Array
-// const boxes = ['a', 'b','c','d','e'];
+// log all pairs of Array
+const boxes = ['a', 'b','c','d','e'];
 
-// function logAllPAirsOfArray(array){
-//     for(let i = 0; i < array.length; i++){
-//         for(let j = 0; j < array.length; j++){
-//             console.log(array[i], array[j]);
-//         }
+function logAllPAirsOfArray(array){
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < array.length; j++){
+            console.log(array[i], array[j]);
+        }
 
-//     }
-// }
-// logAllPAirsOfArray(boxes); //O(n^2)
-
-
-// const array1 = ['a','b', 'c','x'];
-// const array2 = ['z','y', 'x'];
-
-// function containsCommonItem(arr1, arr2){
-//     for(let i=0; i < arr1.length; i++){
-//      for(let j=0; j < arr2.length; j++){
-//          if(arr1[i] === arr2[j]){
-//              return true;
-//          }
-//      }
-//     }
-//     return false;
-// }
-// // O(a*b)
-// O(1) - Space Complexity
-// containsCommonItem(array1, array2);
-
-//===============BETTER SOLUTION:
-
-// array1 == > obj {
-// 
-// a: true
-// b: true
-// c: true
-// x: true
-//}
-// array2[index] === obj.
-// const array1 = ['a','b', 'c','x'];
-// //Compare array2 against array1:
-// const array2 = ['z','y', 'a'];
-// function containsCommonItem2(arr1, arr2){
-
-// // loop thriugh first array and create object(HashTable) where properties === items in the array
-//   let map = {};
-//   for(let i=0; i <arr1.length; i++){
-//     if(!map[array1[i]]){
-//       const item = array1[i];
-//       map[item] = true;
-//     }
-//   }
-// // loop through second array and check if item in second array exists on created object.
-//   for(let j=0; j < array2.length; j++){
-//     if(map[array2[j]]){
-//       return true;
-//     }
-//   }
-//   return false;
-// }
-// // containsCommonItem2(array1, array2);
-
-// // //O(a + b) => Time Complexity
-// // O(a) Space Complexity
-
-// //=======IMPROVE CODE:
-
-// function containsCommonItem3(arr1, arr2){
-//   return arr1.some(item => arr2.includes(item))
-// }
-
-// containsCommonItem3(array1, array2)
+    }
+}
+logAllPAirsOfArray(boxes); //O(n^2)
 
 
-// 
-// Math.pow(6, 1000);
+const array1 = ['a','b', 'c','x'];
+const array2 = ['z','y', 'x'];
 
-// const strings = ['a','b','c','d'];
-// //4*4 = 16 bytes of storage
+function containsCommonItem(arr1, arr2){
+    for(let i=0; i < arr1.length; i++){
+     for(let j=0; j < arr2.length; j++){
+         if(arr1[i] === arr2[j]){
+             return true;
+         }
+     }
+    }
+    return false;
+}
+// O(a*b)
+O(1) - Space Complexity
+containsCommonItem(array1, array2);
 
-// //access the array
-// strings[2] //O(1)
+===============BETTER SOLUTION:
 
-// //push
-// strings.push('e'); // O(1)
+array1 == > obj {
 
-// //pop
-// strings.pop(); //O(1)
-// strings.pop();
+a: true
+b: true
+c: true
+x: true
+}
+array2[index] === obj.
+const array1 = ['a','b', 'c','x'];
+//Compare array2 against array1:
+const array2 = ['z','y', 'a'];
+function containsCommonItem2(arr1, arr2){
 
-// //unshift => add in the front of the array
-// strings.unshift('x'); //O(n) linear
+// loop thriugh first array and create object(HashTable) where properties === items in the array
+  let map = {};
+  for(let i=0; i <arr1.length; i++){
+    if(!map[array1[i]]){
+      const item = array1[i];
+      map[item] = true;
+    }
+  }
+// loop through second array and check if item in second array exists on created object.
+  for(let j=0; j < array2.length; j++){
+    if(map[array2[j]]){
+      return true;
+    }
+  }
+  return false;
+}
+// containsCommonItem2(array1, array2);
 
-// //splice add in the middle of the array
-// strings.splice(2, 0, 'Jazz'); //O(n)
+// //O(a + b) => Time Complexity
+// O(a) Space Complexity
+
+//=======IMPROVE CODE:
+
+function containsCommonItem3(arr1, arr2){
+  return arr1.some(item => arr2.includes(item))
+}
+
+containsCommonItem3(array1, array2)
 
 
-// console.log(strings)
+
+Math.pow(6, 1000);
+
+const strings = ['a','b','c','d'];
+//4*4 = 16 bytes of storage
+
+//access the array
+strings[2] //O(1)
+
+//push
+strings.push('e'); // O(1)
+
+//pop
+strings.pop(); //O(1)
+strings.pop();
+
+//unshift => add in the front of the array
+strings.unshift('x'); //O(n) linear
+
+//splice add in the middle of the array
+strings.splice(2, 0, 'Jazz'); //O(n)
 
 
-// const int = [1, 2, 3, 4, 5];
+console.log(strings)
 
-// int[2] //O(1)
 
-// int.push(10)
+const int = [1, 2, 3, 4, 5];
 
-// // int.pop(4)
+int[2] //O(1)
 
-// int.unshift(290)
+int.push(10)
 
-// int.push(300)
+// int.pop(4)
 
-// console.log(int)
+int.unshift(290)
+
+int.push(300)
+
+console.log(int)
 
 
 class MyArray {
